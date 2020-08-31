29e7d6928d988155777dee8952f8f36e5b96ff11
Nyasha-PC <23294558@sun.ac.za>
2020-08-31 17:18:45 +0200

    Nyasha: some refactoring 17:18

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,14 +1,17 @@
+import javax.print.DocFlavor;
 import java.sql.Connection;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 public class Invoice {
-    private String id;
-    private String issueDate;
-    private double sum;
+    private final String id;
+    private final String issueDate;
+    private final double sum;
+    private final ArrayList<Product> products;
 
     public Invoice(Order order) {
-        issueDate = LocalDateTime.now().toString().replaceAll("T", " ");
+        issueDate = LocalDateTime.now().toString().replaceAll("T", " ").substring(0, 19);
+        products = order.getProducts();
         id = order.getOrderID();
         sum = order.getSum();
     }
@@ -25,6 +28,17 @@ public class Invoice {
         return sum;
     }
 
+    public String printInvoice() {
+        StringBuilder s = new StringBuilder();
+        s.append(String.format("\n| %s | %s-20.20s| %f,2d|", id, issueDate, sum));
+        for (Product product : products) {
+            s.append(String.format("\n|  | %-20.20s|         |",
+                    product.getId()+" "+product.getProductTitle()+" "+product.getPrice()
+            ));
+        }
+        return s.toString();
+    }
+
     @Override
     public String toString() {
         return "Invoice{" +
@@ -38,6 +52,6 @@ public class Invoice {
         Connection con = new MYSQLConnection().getDBConnection();
         ArrayList<Order> orderList = new Order(con, "6").queryOrders();
         Invoice n = new Invoice(orderList.get(0));
-        System.out.println(n.toString());
+        System.out.println(n.printInvoice());
     }
 }
